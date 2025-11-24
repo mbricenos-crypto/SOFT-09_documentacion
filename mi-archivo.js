3eab57122483568b748bbb8c7197f214a57bb964
Briceno <manfsanc@amazon.com>
2025-11-23 22:41:46 -0600

    Este es un nuevo cambio aplicado

---
+++ mi-archivo.js
@@ -0,0 +1,14 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.printIn("Hola, mundo desde Java!");
+        saludar("nombre");
+        SVGAnimatedEnumeration(5,3);
+    }
+    public static void saludar(String nombre){
+        System.out.printIn("Hola,"+ nombre + ". Bienvenida al ejercicio de Git y Github!");
+    }
+    public static void sumar(int a, int b){
+        int resultado = a+b;
+        System.out.printIn("La suma de" + a + "+" + b + "es:" + resultado);
+    }
+}
